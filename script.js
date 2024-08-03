d8c7bab14022afc09e53a686fce94220d847b14d
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-03 10:57:33 -0300

    att

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const desligar = () => {
-    $('#imagem').attr('src', './apagada.jpeg');
-    $('#corpo').css('background', 'black');
-    $('body').css('background', 'black');
-    $('#corpo').removeClass('ligada').addClass('desligada');
-};
-
-const ligar = () => {
-    $('#imagem').attr('src', './acesa.jpeg');
-    $('#corpo').css('background', 'white');
-    $('body').css('background', 'white');
-    $('#corpo').removeClass('desligada').addClass('ligada');
-};
-
-$('#toggles').change(function() {
+$('input').change(function () {
     if (this.checked) {
-        ligar();
+        $('body').css('background', 'white');
+        $('#corpo').css('background', 'white');
+        $('img').attr('src', './acesa.jpeg');
+
+        
     } else {
-        desligar();   
+        $('body').css('background', 'black');
+        $('#corpo').css('background', 'black');
+        $('img').attr('src', './apagada.jpeg');
+       
     }
-});
+})
