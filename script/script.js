efa21400786611a63fc6cf08d15c1759cf396c8d
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-04 13:27:47 -0300

    a

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ $('input').change(function () {
     if (this.checked) {
         $('body').css('background', 'white');
         $('#corpo').css('background', 'white');
+        $('h1').css('color', 'black');
         $('img').attr('src', './images/acesa.jpeg');
-
+        
         
     } else {
         $('body').css('background', 'black');
         $('#corpo').css('background', 'black');
+        $('h1').css('color', 'rgb(100 116 139)');
         $('img').attr('src', './images/apagada.jpeg');
        
     }
