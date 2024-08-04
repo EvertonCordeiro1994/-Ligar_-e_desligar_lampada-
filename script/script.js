efe979938b855c4b31ed4c9b847b328583801b55
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-04 13:39:38 -0300

    bah

--- script/script.js
+++ script/script.js
@@ -1,16 +1,15 @@
-$('input').change(function () {
-    if (this.checked) {
-        $('body').css('background', 'white');
-        $('#corpo').css('background', 'white');
-        $('h1').css('color', 'black');
-        $('img').attr('src', 'https://raw.githubusercontent.com/EvertonCordeiro1994/lampadaonoff/main/images/acesa.JPEG');
-        
-        
-    } else {
-        $('body').css('background', 'black');
-        $('#corpo').css('background', 'black');
-        $('h1').css('color', 'rgb(100 116 139)');
-        $('img').attr('src', 'https://raw.githubusercontent.com/EvertonCordeiro1994/lampadaonoff/main/images/apagada.JPEG');
-       
-    }
-})
+function ligar(){
+    $('body').css('background', 'white');
+    $('#corpo').css('background', 'white');
+    $('h1').css('color', 'black');
+    $('img').attr('src', './images/acesa.jpeg');
+}
+
+function desligar(){
+    $('body').css('background', 'black');
+    $('#corpo').css('background', 'black');
+    $('h1').css('color', 'rgb(100 116 139)');
+    $('img').attr('src', './images/apagada.jpeg');
+}
+
+$('input').change( function () {this.checked ? ligar() : desligar()})
